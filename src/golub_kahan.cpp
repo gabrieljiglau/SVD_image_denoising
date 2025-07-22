aba4f44907edfd8683826e53d6e8115e7a7ce4b5
gabriel <gabriel_jiglau@yahoo.com>
2025-07-22 23:01:22 +0300

    start golub kahan

--- src/golub_kahan.cpp
+++ src/golub_kahan.cpp
@@ -32,28 +32,58 @@ Eigen::MatrixXd chooseSubmatrix(Eigen::MatrixXd B){
     return B.block(B.rows() - 2, B.cols() - 2, 2, 2);
 }
 
-double wilkinsonShift(Eigen::MatrixXd B){
+double wilkinsonShift(Eigen::MatrixXd trail){
 
-    double a = B(0, 0);
-    double b = B(0, 1);
-    double c = B(1, 1);
+    double a = trail(0, 0);
+    double b = trail(0, 1);
+    double c = trail(1, 1);
 
-    double delta = (a - c) / 2;
-    double sign = (delta > 0) ? 1.0 : -1.0;
+    double delta = (a - c) / 2;    
+    double denominator = std::sqrt(std::pow(delta, 2) + std::pow(b, 2)) + std::abs(delta);
+
+    double sign = (delta >= 0) ? 1.0 : 0.0;
+    return  c - (sign * std::pow(b, 2)) / denominator;
+}
+
+Eigen::MatrixXd shiftHelper(Eigen::MatrixXd A, double miu){
+
+    double first = std::pow(A(0, 0), 2) - miu;
+    double second = A(0, 0) *A(1, 0);
+
+    double radius = std::sqrt(std::pow(first, 2) + std::pow(second, 2));
+    double cos = first / radius;
+    double sin = second / radius;
+
+    Eigen::MatrixXd G = Eigen::MatrixXd(2, 2);
+    G(0,0) = cos;
+    G(0, 1) = sin;
+    G(1, 0) = -sin;
+    G(1, 1) = cos;
+
+    return G;
+}
+
+Eigen::MatrixXd applyShift(Eigen::MatrixXd trail, double miu, Eigen::MatrixXd &U, Eigen::MatrixXd &V_transposed){
+
+    Eigen::MatrixXd G = shiftHelper(trail, miu);
+    U = G * U;
+    Eigen::MatrixXd newTrail = trail * G.transpose();
     
-    double denominator = std::sqrt(std::pow(delta, 2) + std::pow(b, 2) + std::abs(delta));
-    return c - (sign * std::pow(b, 2)) / denominator;
+    G = shiftHelper(newTrail, miu);
+    V_transposed = V_transposed * U;
+    return G.transpose() * newTrail;
 }
 
 Eigen::MatrixXd golubKahan(Eigen::MatrixXd U, Eigen::MatrixXd &B, Eigen::MatrixXd V_transposed){
 
+    B = B * B.transpose();
     while (!isDiagonal(B)){
 
-        B = B * B.transpose();
-
         Eigen::MatrixXd trail = chooseSubmatrix(B);
-        double miu = wilkinsonShift(B);
+        double miu = wilkinsonShift(trail);
 
-        applyShift(B, miu);
+        // the trail doesn't need to be another variable
+        // and the result needs to be updated into B;
+        applyShift(trail, miu, U, V_transposed);
     }
 }
--- src/golub_kahan.hpp
+++ src/golub_kahan.hpp
@@ -9,4 +9,6 @@ Eigen::MatrixXd chooseSubmatrix(Eigen::MatrixXd B);
 
 double wilkinsonShift(Eigen::MatrixXd B);
 
+Eigen::MatrixXd applyShift(Eigen::MatrixXd trail, double miu);
+
 Eigen::MatrixXd golubKahan(Eigen::MatrixXd U, Eigen::MatrixXd &B, Eigen::MatrixXd V_transposed);
